f1037e61e161597c1b3e35a8677816cf5b090900
foobar42fsm <webelliot@fordcochrane.com>
2018-03-16 00:49:08 -0400

    added the VGA module to top

--- gameboard.v
+++ gameboard.v
@@ -1,3 +1,56 @@
+module gameboard_top(
+    input CLOCK_50,         // On Board 50 MHz
+    // Board inputs
+    input [9:0] KEY,
+    // The ports below are for the VGA output.
+    output VGA_CLK,         // VGA Clock
+    output VGA_HS,          // VGA H_SYNC
+    output VGA_VS,          // VGA V_SYNC
+    output VGA_BLANK_N,     // VGA BLANK
+    output VGA_SYNC_N,      // VGA SYNC
+    output [9:0] VGA_R,     // VGA Red[9:0]
+    output [9:0] VGA_G,     // VGA Green[9:0]
+    output [9:0] VGA_B     // VGA Blue[9:0]
+  );
+
+    wire resetn;
+    assign resetn = KEY[0];
+
+    /*
+     * Create an instance of a VGA controller
+     * Define the number of colors as well as the initial background
+     * image file (.MIF) for the controller
+     */
+    vga_adapter VGA(
+      .reset(resetn),
+      .clock(CLOCK_50),
+      .color(color),
+      .x(x),
+      .y(y),
+      .plot(writeEn),
+      // Signals for the DAC to drive the monitor
+      .VGA_R(VGA_R),
+      .VGA_G(VGA_G),
+      .VGA_B(VGA_B),
+      .VGA_HS(VGA_HS),
+      .VGA_VS(VGA_VS),
+      .VGA_BLANK(VGA_BLANK_N),
+      .VGA_SYNC(VGA_SYNC_N),
+      .VGA_CLK(VGA_CLK)
+    );
+    defparam VGA.RESOLUTION = "160X120";
+    defparam VGA.MONOCHROME = "FALSE";
+    defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+    defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+    defparam VGA.BACKGROUND_IMAGE = "gameboard.mif";
+
+
+
+
+
+
+endmodule
+
 module gameboard(
   input clk,
   input [63:0] mineMap,
