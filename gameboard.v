3110113eef7cbc5a7cf70a0771b588c9ffe45031
Jingyuan Gao <rooney.gao@mail.utoronto.ca>
2018-03-16 02:53:01 -0400

    implemented tile_control to work with datapath

--- gameboard.v
+++ gameboard.v
@@ -146,3 +146,114 @@ module y_counter(
 		end
 	end
 endmodule
+
+module tile_control(
+  input clk,
+  input resetn,
+  input go,
+
+  output reg ld_x, ld_y, ld_color,
+  output reg en_xc,
+  output reg writeEn);
+
+  reg [4:0] current_state, next_state;
+
+  localparam  S_LOAD      = 5'd0,
+              S_LOAD_WAIT = 5'd1,
+              S_CYCLE_0   = 5'd2,
+              S_CYCLE_1   = 5'd3,
+              S_CYCLE_2   = 5'd4,
+              S_CYCLE_3   = 5'd5,
+              S_CYCLE_4   = 5'd6,
+              S_CYCLE_5   = 5'd7,
+              S_CYCLE_6   = 5'd8,
+              S_CYCLE_7   = 5'd9,
+              S_CYCLE_8   = 5'd10,
+              S_CYCLE_9   = 5'd11,
+              S_CYCLE_10  = 5'd12,
+              S_CYCLE_11  = 5'd13,
+              S_CYCLE_12  = 5'd14,
+              S_CYCLE_13  = 5'd15,
+              S_CYCLE_14  = 5'd16,
+              S_CYCLE_15  = 5'd17;
+
+  // Next state logic aka our state table
+  always@(*)
+  begin: state_table
+    case (current_state)
+      S_LOAD: next_state = go ? S_LOAD_WAIT : S_LOAD; // Loop in current state until value is input
+      S_LOAD_WAIT: next_state = go ? S_LOAD_WAIT : S_CYCLE_0; // Loop in current state until go signal goes low
+      S_CYCLE_0: next_state = S_CYCLE_1;
+      S_CYCLE_1: next_state = S_CYCLE_2;
+      S_CYCLE_2: next_state = S_CYCLE_3;
+      S_CYCLE_3: next_state = S_CYCLE_4;
+      S_CYCLE_4: next_state = S_CYCLE_5;
+      S_CYCLE_5: next_state = S_CYCLE_6;
+      S_CYCLE_6: next_state = S_CYCLE_7;
+      S_CYCLE_7: next_state = S_CYCLE_8;
+      S_CYCLE_8: next_state = S_CYCLE_9;
+      S_CYCLE_9: next_state = S_CYCLE_10;
+      S_CYCLE_10:next_state = S_CYCLE_11;
+      S_CYCLE_11:next_state = S_CYCLE_12;
+      S_CYCLE_12:next_state = S_CYCLE_13;
+      S_CYCLE_13:next_state = S_CYCLE_14;
+      S_CYCLE_14:next_state = S_CYCLE_15;
+      S_CYCLE_15:next_state = S_LOAD;
+      default: next_state = S_LOAD;
+    endcase
+  end
+
+  // Output logic aka all of our datapath control signals
+  always @(*)
+  begin: enable_signals
+    // By default make all our signals 0
+    ld_x = 1'b0;
+    ld_y = 1'b0;
+		en_xc = 1'b0;
+		en_yc = 1'b0;
+    ld_color = 1'b0;
+    writeEn = 1'b0;
+
+    case (current_state)
+      S_LOAD: begin
+        ld_x = 1'b1;
+        ld_y = 1'b1;
+        ld_color = 1'b1;
+        end
+      S_CYCLE_0,  // row 1
+      S_CYCLE_1,  // row 1
+      S_CYCLE_2,   // row 1
+      S_CYCLE_4,  // row 2
+      S_CYCLE_5,  // row 2
+      S_CYCLE_6,  // row 2
+      S_CYCLE_8,  // row 3
+      S_CYCLE_9,  // row 3
+      S_CYCLE_10, // row 3
+      S_CYCLE_12, // row 4
+      S_CYCLE_13, // row 4
+      S_CYCLE_14: // row 4
+      begin // start shift across the x axis
+				en_xc = 1'b1;
+        writeEn = 1'b1;
+        end
+      S_CYCLE_3, // row 1
+      S_CYCLE_7, // row 2
+      S_CYCLE_11,// row 3
+      S_CYCLE_15:// row 4
+      begin //  start shift down the y axis
+        ld_x = 1'b1;
+        ld_y
+				en_yc = 1'b1;
+        writeEn = 1'b1;
+        end
+    endcase
+  end
+
+  always@(posedge clk)
+  begin: state_FFs
+    if(!resetn)
+      current_state <= S_LOAD;
+    else
+      current_state <= next_state;
+  end
+  endmodule
